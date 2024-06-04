bad05e091951f4eeb6004d3e01b18f8950d64711
soliha02 <164037162+soliha02@users.noreply.github.com>
2024-06-04 21:29:05 +0500

    Add files via upload

---
+++ bs.js
@@ -0,0 +1,8 @@
+let ey = prompt('Ismga : Bobur , Harfga A kiriting!!!')
+let oq = prompt('Ismingizni kiriting')
+let qora = prompt('Harf kiriting')
+let ha =  prompt('Bobur ismning ichida A harifi mavjud emas!!!')
+alert(oq,qora,ey)
+
+
+
